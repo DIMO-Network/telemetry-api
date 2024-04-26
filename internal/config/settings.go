c0251697d2ec32239b854c0c3195d695347ef976
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-04-26 10:29:24 -0400

    Remove devices-api setting

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -8,7 +8,6 @@ type Settings struct {
 	ClickHouseTCPPort         int    `yaml:"CLICKHOUSE_TCP_PORT"`
 	ClickHouseUser            string `yaml:"CLICKHOUSE_USER"`
 	ClickHousePassword        string `yaml:"CLICKHOUSE_PASSWORD"`
-	DevicesAPIGRPCAddr        string `yaml:"DEVICES_APIGRPC_ADDR"`
 	TokenExchangeJWTKeySetURL string `yaml:"TOKEN_EXCHANGE_JWK_KEY_SET_URL"`
 	TokenExchangeIssuer       string `yaml:"TOKEN_EXCHANGE_ISSUER_URL"`
 	VehicleNFTAddress         string `yaml:"VEHICLE_NFT_ADDRESS"`
