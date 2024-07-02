bdb07bfe3b627fbbda7890f0263b6f264801456b
Dylan Moreland <dylan@dimo.zone>
2024-07-02 13:38:51 -0400

    Docs on models

--- internal/graph/model/signal_aggs.go
+++ internal/graph/model/signal_aggs.go
@@ -2,11 +2,16 @@ package model
 
 import "time"
 
+// AliasKey identifies the combination of signal name and aggregation
+// for a certain time bucket.
 type AliasKey struct {
 	Name string
 	Agg  string
 }
 
+// SignalAggregations is the Go struct corresponding to the GraphQL type
+// SignalAggregations. Most of its subfields are signal aggregation results
+// that are returned by resolvers, so they do not appear on the model.
 type SignalAggregations struct {
 	Timestamp time.Time `json:"timestamp"`
 
@@ -14,6 +19,9 @@ type SignalAggregations struct {
 	ValueStrings map[AliasKey]string  `json:"-"`
 }
 
+// AggSignal holds the value of an aggregation for a signal in a certain
+// time bucket. Only one of ValueNumber and ValueString contains a meaningful
+// value, determined by Name.
 type AggSignal struct {
 	Name        string
 	Agg         string
