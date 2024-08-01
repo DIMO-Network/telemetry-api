c6c405bd35eaa4e27106d830a474fd8106dbf903
Dylan Moreland <dylan@dimo.zone>
2024-08-01 09:34:27 -0400

    Try to get some control over the error handling

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"strconv"
 
@@ -12,7 +11,31 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-var errUnauthorized = errors.New("unauthorized")
+type UnauthorizedError struct {
+	message string
+	err     error
+}
+
+func (e UnauthorizedError) Error() string {
+	if e.message != "" {
+		if e.err != nil {
+			return fmt.Sprintf("unauthorized: %s: %s", e.message, e.err)
+		}
+		return fmt.Sprintf("unauthorized: %s", e.message)
+	}
+	if e.err != nil {
+		return fmt.Sprintf("unauthorized: %s", e.err)
+	}
+	return "unauthorized"
+}
+
+func (e UnauthorizedError) Unwrap() error {
+	return e.err
+}
+
+func newError(msg string, args ...any) error {
+	return UnauthorizedError{message: fmt.Sprintf(msg, args...)}
+}
 
 var vehiclePrivToAPI = map[privileges.Privilege]model.Privilege{
 	privileges.VehicleNonLocationData: model.PrivilegeVehicleNonLocationData,
@@ -32,23 +55,28 @@ func CreateVehicleTokenCheck(contractAddr string) func(context.Context, any, gra
 	return func(ctx context.Context, _ any, next graphql.Resolver) (any, error) {
 		claim, err := getTelemetryClaim(ctx)
 		if err != nil {
-			return nil, fmt.Errorf("%s: %w", errUnauthorized.Error(), err)
+			return nil, UnauthorizedError{err: err}
 		}
 
 		if claim.ContractAddress != requiredAddr {
-			return nil, fmt.Errorf("%w: contract did not match", errUnauthorized)
+			return nil, newError("contract in claim is %s instead of the required %s", claim.ContractAddress, requiredAddr)
 		}
 
 		fCtx := graphql.GetFieldContext(ctx)
 		if fCtx == nil {
-			return nil, fmt.Errorf("%w: no field context found", errUnauthorized)
+			return nil, newError("no field context")
 		}
-		tokenID, ok := fCtx.Args[tokenIdArgName].(int)
+		tokenIDAny, ok := fCtx.Args[tokenIdArgName]
 		if !ok {
-			return nil, fmt.Errorf("%w: failed to get %s from args", errUnauthorized, tokenIdArgName)
+			return nil, newError("no argument named %s", tokenIdArgName)
+		}
+		tokenID, ok := tokenIDAny.(int)
+		if !ok {
+			return nil, newError("argument %s has type %T instead of the expected %T", tokenIdArgName, tokenIDAny, tokenID)
+
 		}
 		if strconv.Itoa(tokenID) != claim.TokenID {
-			return nil, fmt.Errorf("%w: %s mismatch", errUnauthorized, tokenIdArgName)
+			return nil, newError("token id %s in the claim does not match token id %d in the query", claim.TokenID, tokenID)
 		}
 
 		return next(ctx)
@@ -59,12 +87,12 @@ func CreateVehicleTokenCheck(contractAddr string) func(context.Context, any, gra
 func PrivilegeCheck(ctx context.Context, _ any, next graphql.Resolver, privs []model.Privilege) (any, error) {
 	claim, err := getTelemetryClaim(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", errUnauthorized.Error(), err)
+		return nil, UnauthorizedError{err: err}
 	}
 
 	for _, priv := range privs {
 		if !claim.privileges.Contains(priv) {
-			return nil, fmt.Errorf("%w: missing required privilege %s", errUnauthorized, priv)
+			return nil, newError("missing required privilege %s", priv)
 		}
 	}
 
