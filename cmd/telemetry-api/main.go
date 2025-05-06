c64b2a0baee29259ad64b50e04e12dfed90aeb2a
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-05-06 11:09:37 -0400

    Update profile endpoints

--- cmd/telemetry-api/main.go
+++ cmd/telemetry-api/main.go
@@ -66,10 +66,10 @@ func serveMonitoring(port string, logger *zerolog.Logger, enablePprof bool) *fib
 
 	// Add pprof handlers if enabled
 	if enablePprof {
-		pprofGroup := monApp.Group("/debug/pprof")
+		pprofGroup := monApp.Group("/debug")
 
 		// Index page and base profiles
-		pprofGroup.Get("/", adaptor.HTTPHandlerFunc(pprof.Index))
+		pprofGroup.Get("/pprof", adaptor.HTTPHandlerFunc(pprof.Index))
 		pprofGroup.Get("/cmdline", adaptor.HTTPHandlerFunc(pprof.Cmdline))
 		pprofGroup.Get("/profile", adaptor.HTTPHandlerFunc(pprof.Profile))
 		pprofGroup.Get("/symbol", adaptor.HTTPHandlerFunc(pprof.Symbol))
