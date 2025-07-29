4b5371724beda442eaead19f43f10c046b83817a
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-07-29 08:29:44 -0400

    feat: add a detailed query cost calculations and estimator

---
+++ e2e/cost_estimator_test.go
@@ -0,0 +1,65 @@
+package e2e_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/99designs/gqlgen/client"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEstimateCost(t *testing.T) {
+	services := GetTestServices(t)
+	// Create and set up GraphQL server
+	telemetryClient := NewGraphQLServer(t, services.Settings)
+
+	// Create auth token for vehicle
+	token := services.Auth.CreateVehicleToken(t, "39718", []int{1})
+
+	// Execute the query
+	query := `
+	query Latest_all {
+		smartcar: signalsLatest(filter: {source: "smartcar"}, tokenId: 39718) {
+			lastSeen
+			speed {
+				timestamp
+				value
+			}
+			s1: speed {
+				timestamp
+				value
+			}
+			dimoAftermarketWPAState {
+				timestamp
+				value
+			}
+			currentLocationHeading {
+				timestamp
+				value
+			}
+			currentLocationIsRedacted {
+				timestamp
+				value
+			}
+			loc2: currentLocationIsRedacted {
+				timestamp
+				value
+			}
+				
+
+		}
+	
+	}`
+
+	// Execute request
+	result, err := telemetryClient.RawPost(query, WithToken(token), WithEstimateCost())
+	require.NoError(t, err)
+	json, err := json.MarshalIndent(result, "", "  ")
+	require.NoError(t, err)
+	fmt.Println(string(json))
+}
+
+func WithEstimateCost() client.Option {
+	return client.AddHeader("X-Estimate-Cost", "true")
+}
