c559fbe9d63d4a44dc68e79b10b67d7d1baecadf
Dylan Moreland <dylan@dimo.zone>
2026-04-21 11:16:57 -0400

    Remove this unused VIN VC dataversion setting
    I believe this is from when these had their own type.

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -7,22 +7,19 @@ import (
 
 // Settings contains the application config.
 type Settings struct {
-	LogLevel                     string          `yaml:"LOG_LEVEL"`
-	Port                         int             `yaml:"PORT"`
-	MonPort                      int             `yaml:"MON_PORT"`
-	EnablePprof                  bool            `yaml:"ENABLE_PPROF"`
-	Clickhouse                   config.Settings `yaml:",inline"`
-	TokenExchangeJWTKeySetURL    string          `yaml:"TOKEN_EXCHANGE_JWK_KEY_SET_URL"`
-	TokenExchangeIssuer          string          `yaml:"TOKEN_EXCHANGE_ISSUER_URL"`
-	VehicleNFTAddress            common.Address  `yaml:"VEHICLE_NFT_ADDRESS"`
-	MaxRequestDuration           string          `yaml:"MAX_REQUEST_DURATION"`
-	LatestSignalsLookbackDays    int64           `yaml:"LATEST_SIGNALS_LOOKBACK_DAYS"`
-	ChainID                      uint64          `yaml:"DIMO_REGISTRY_CHAIN_ID"`
-	FetchAPIGRPCEndpoint         string          `yaml:"FETCH_API_GRPC_ENDPOINT"`
-	CreditTrackerEndpoint        string          `yaml:"CREDIT_TRACKER_ENDPOINT"`
-	StorageNodeDevLicense        common.Address  `yaml:"STORAGE_NODE_DEV_LICENSE"`
-	VINDataVersion               string          `yaml:"VIN_DATA_VERSION"`
-
-	// TODO: remove these once manual vinvc are migrated to attestation
-	VINVCDataVersion string `yaml:"VINVC_DATA_VERSION"`
+	LogLevel                  string          `yaml:"LOG_LEVEL"`
+	Port                      int             `yaml:"PORT"`
+	MonPort                   int             `yaml:"MON_PORT"`
+	EnablePprof               bool            `yaml:"ENABLE_PPROF"`
+	Clickhouse                config.Settings `yaml:",inline"`
+	TokenExchangeJWTKeySetURL string          `yaml:"TOKEN_EXCHANGE_JWK_KEY_SET_URL"`
+	TokenExchangeIssuer       string          `yaml:"TOKEN_EXCHANGE_ISSUER_URL"`
+	VehicleNFTAddress         common.Address  `yaml:"VEHICLE_NFT_ADDRESS"`
+	MaxRequestDuration        string          `yaml:"MAX_REQUEST_DURATION"`
+	LatestSignalsLookbackDays int64           `yaml:"LATEST_SIGNALS_LOOKBACK_DAYS"`
+	ChainID                   uint64          `yaml:"DIMO_REGISTRY_CHAIN_ID"`
+	FetchAPIGRPCEndpoint      string          `yaml:"FETCH_API_GRPC_ENDPOINT"`
+	CreditTrackerEndpoint     string          `yaml:"CREDIT_TRACKER_ENDPOINT"`
+	StorageNodeDevLicense     common.Address  `yaml:"STORAGE_NODE_DEV_LICENSE"`
+	VINDataVersion            string          `yaml:"VIN_DATA_VERSION"`
 }
